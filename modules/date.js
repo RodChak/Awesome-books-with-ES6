fca66dd076f4703373c6aff7516c8f1ba2e1ad50
RodChak <97179349+RodChak@users.noreply.github.com>
2022-08-10 15:07:15 +0200

    add modules
    Signed-off-by: RodChak <97179349+RodChak@users.noreply.github.com>

---
+++ modules/date.js
@@ -0,0 +1,11 @@
+
+import { DateTime } from './luxon.min.js';
+// to display date
+export default class X {
+  static displayDate() {
+    const date = DateTime.now().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
+    return date
+  } 
+};
+
+// export default displayDate;
